1d6e9c5477e075a2be0e6340f1b74569c3e1f599
johannes misensky <johannes.misensky@hotmail.com>
2023-06-10 11:04:40 +0200

    dunno

--- app/models.py
+++ app/models.py
@@ -1,48 +1,49 @@
 from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
 
 from datetime import datetime
+db = SQLAlchemy()
 
+# class NilmModel(db.Model):
+#     """
+#     A class of the NILM models that have been pre-trained in the first part
+#     """
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(50), unique=True, nullable=False)
+#     description = Column(String(100), unique=True, nullable=True)
 
-class NilmModel(Model):
-    """
-    A class of the NILM models that have been pre-trained in the first part
-    """
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True, nullable=False)
-    description = Column(String(100), unique=True, nullable=True)
+#     def __repr__(self) -> str:
+#         return self.name
 
-    def __repr__(self) -> str:
-        return self.name
+# class Appliance(db.Model):
+#     """
+#     A class representing an appliance that was modelled in the first part
+#     """
+#     id = Column(Integer, primary_key=True)
+#     label = Column(String(50), unique=True, nullable=False)
 
-class Appliance(Model):
-    """
-    A class representing an appliance that was modelled in the first part
-    """
-    id = Column(Integer, primary_key=True)
-    label = Column(String(50), unique=True, nullable=False)
+#     def __repr__(self) -> str:
+#         return self.label
 
-    def __repr__(self) -> str:
-        return self.label
+# class ModelInstance(db.Model):
+#     """
+#     A class representing an instance of the model included in NilmModel
+#     """
+#     id_appliance = Column(Integer, ForeignKey('appliance.id'), nullable = False)
+#     appliance = relationship('Appliance')
+#     id_model = Column(Integer, ForeignKey('nilmModel.id'), nullable = False)
+#     model = relationship('NilmModel')
+#     path_weights = Column(String(50), unique=True, nullable=True)
+#     mean = Column(float, nullable=False)
+#     std = Column(float, nullable=False)
+#     metadata = Column(String(500), unique=True, nullable=True)
 
-class ModelInstance(Model):
-    """
-    A class representing an instance of the model included in NilmModel
-    """
-    id_appliance = Column(Integer, ForeignKey('appliance.id'), nullable = False)
-    appliance = relationship('Appliance')
-    id_model = Column(Integer, ForeignKey('nilmModel.id'), nullable = False)
-    model = relationship('NilmModel')
-    path_weights = Column(String(50), unique=True, nullable=True)
-    mean = Column(float, nullable=False)
-    std = Column(float, nullable=False)
-    metadata = Column(String(500), unique=True, nullable=True)
+#     def __repr__(self) -> str:
+#         return super().__repr__()
 
-    def __repr__(self) -> str:
-        return super().__repr__()
-
-class History(Model):
+class History(db.Model):
     """
     A class to save the hsitorical data of the users
     """
